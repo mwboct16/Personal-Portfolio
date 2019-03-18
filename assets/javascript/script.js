dd225c25c0d2770de9c2be13b7c029389e5e5e35
Michael Black <mwboct16@gmail.com>
2019-03-17 20:11:31 -0400

    Re-organized path tree because github was deploying readME

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,7 @@
+/* APIs to be used:
+- MailChimp
+- Firebase
+- Google Maps (Possibly)
+- Google Hangout (Possibly)
+*/
 
-// var queryURLMailchimp = "https://us20.api.mailchimp.com/3.0"
-
-
-// // Mailchimp 
-// $.ajax({
-//     method: "GET",
-//     url: queryURLMailchimp
-// }).then(function(response){
-//     console.log(response);
-// })
